004813685bf8d88d4b608f4bdd7db19811126eb5
perichor <sandejeremy@gmail.com>
2017-01-04 20:00:26 -0800

    implement adding songs to queue

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -12,6 +12,10 @@ var AppModel = Backbone.Model.extend({
     end up referring to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
 
+    params.library.on('enqueue', function(song) {
+      this.get('songQueue').add(song);
+      console.log(this.get('songQueue'));
+    }, this);
 
     params.library.on('play', function(song) {
       this.set('currentSong', song);
