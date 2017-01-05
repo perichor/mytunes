7560eed05502253f62e55a5566203727ac62a901
perichor <sandejeremy@gmail.com>
2017-01-05 15:22:22 -0800

    implement parse api library

--- client/data/data.js
+++ client/data/data.js
@@ -1,23 +1,23 @@
 // data.js - Defines an array of data regarding song files and their accompanying details.
-var songData = [
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3',
-    title: 'One In A Million',
-    artist: 'Aaliyah',
-  },
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3',
-    title: 'Age Ain\'t Nothing But A Number',
-    artist: 'Aaliyah',
-  },
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3',
-    title: 'Hot Like Fire',
-    artist: 'Aaliyah',
-  },
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3',
-    title: 'If Your Girl Only Knew',
-    artist: 'Aaliyah',
-  }
-];
+
+var songDataGET = function() {
+  return $.ajax({
+    url: 'https://api.parse.com/1/classes/songs/',
+    type: 'GET',
+    // data: ,
+    success: function(data) {
+      // set up model objects
+      console.log(data);
+      var library = new Songs(data.results);
+      var app = new AppModel({library: library});
+
+      // build a view for the top level of the whole app
+      var appView = new AppView({model: app});
+
+      // put the view onto the screen
+      $('body').append(appView.render());
+    }
+  });
+};
+
+songDataGET();
